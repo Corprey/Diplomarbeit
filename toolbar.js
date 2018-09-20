7462c4d50f2388ef7b63b13eefc8d4f4a8eb4e64
PreyMa <mlpreymann@gmail.com>
2018-09-20 12:17:24 +0200

    Toolbar RadioButtons
    - Added radio button capabilities to the tool bar

--- toolbar.js
+++ toolbar.js
@@ -17,6 +17,7 @@ function ToolbarIcon(cnf) {
   this.iconType= config.iconType;
   this.iconImg= config.iconImg;
   this.action= config.action;
+  this.id= config.id;
 
   this.toolIcon= document.createElement("span");    // create button Icon element
   this.toolIcon.classList.add(this.iconType);       // set css class
@@ -46,6 +47,55 @@ ToolbarIcon.prototype.attachTo= function(anker) {
 
 
 /*
+* RadioButton Class managing the activation and deactivation of its connections
+* Interally it holds a ToolbarIcon as a mixin
+*/
+function RadioButton( cnf, menu ) {
+  // override the event callback to additionally call the update method of the button
+  let self= this;
+  let action= cnf.action;
+  cnf.action= function() { self.update(); action(); };
+
+  this.menu= menu;
+  this.connections= cnf.connections;
+
+  this.icon= new ToolbarIcon( cnf );
+  cnf.defaultEnabled ? this.enable() : this.disable();
+}
+
+// Enable this button and disable all connected buttons
+RadioButton.prototype.update= function() {
+  // disable all connected buttons
+  for( let i= 0; i!= this.connections.length; i++ ) {
+    let ic= this.menu.getById( this.connections[i] );
+
+    if( ic === null ) {
+      console.error( "Connection to non existing icon." );
+    } else {
+      ic.disable();
+    }
+  }
+
+  // enable button
+  this.enable();
+}
+
+RadioButton.prototype.disable= function() {
+  // CSS class ändern
+  // this.icon.button.classList.add();
+}
+
+RadioButton.prototype.enable= function() {
+  // CSS class ändern
+}
+
+RadioButton.prototype.attachTo= function( x ) {
+  this.icon.attachTo( x );
+}
+
+
+
+/*
 * Toolbar Class holding all buttons
 */
 function Toolbar(name, arr) {
@@ -57,9 +107,30 @@ function Toolbar(name, arr) {
   }
 
   this.createToolbarIcon= function(cnf) {
-    let t= new ToolbarIcon(cnf);
+    let t;
+
+    if( cnf.type == 'radio' ) {
+      t= new RadioButton( cnf, this )
+    } else {
+      t= new ToolbarIcon(cnf);
+    }
+
     this.icons.push( t );
     t.attachTo( this.anker );
+  }
+
+  this.getById= function( id ) {
+    // Iterate through all icons and return the first with
+    // ... matching id
+    for( let i= 0; i!= this.icons.length; i++ ) {
+      let icon= this.icons[i];
+
+      if( icon.id === id ) {
+        return icon;
+      }
+    }
+
+    return null;
   }
 }
 
